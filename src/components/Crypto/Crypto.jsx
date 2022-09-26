d3bea3db91623c178c534b03cb34a6cfeada2dd7
Valerii <intervention242424@gmail.com>
2022-09-26 13:27:57 +0300

    added: component crypto

---
+++ src/components/Crypto/Crypto.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+class Crypto extends Component {
+    state = {
+
+    }
+
+    render(){
+        return(
+            <></>
+        )
+    }
+}
+
+export default Crypto
